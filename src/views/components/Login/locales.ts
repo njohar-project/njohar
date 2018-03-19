11b934abe7e87469e610103a8c454c6948a31986
tabah setyo aji <tabah.setyo.aji@gmail.com>
2018-03-19 16:07:19 +0700

    Added login component

--- src/views/components/Login/locales.ts
+++ src/views/components/Login/locales.ts
@@ -1,9 +1,19 @@
 const locales = {
   'en-US': {
-    login: 'Login'
+    login: 'Login',
+    usernameIsRequired : 'Please input your username.',
+    passwordIsRequired : 'Please input your password.',
+    usernamePlaceholder : 'Username',
+    passwordPlaceholder : 'Password',
+    forget : 'Forgot password',
   },
   'id-ID': {
-    login: 'Masuk'
+    login: 'Masuk',
+    usernameIsRequired : 'Silakan isi nama pengguna anda.',
+    passwordIsRequired : 'Silakan isi kata sandi anda.',
+    usernamePlaceholder : 'Nama Pengguna',
+    passwordPlaceholder : 'Kata Sandi',
+    forget : 'Lupa kata sandi',
   }
 }
 
